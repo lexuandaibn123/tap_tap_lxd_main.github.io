ceefc1033d0f768e10ea9821f4df0a28a7913c6a
lexuandaibn123 <118295416+lexuandaibn123@users.noreply.github.com>
2023-01-20 13:48:08 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -1,20 +1,33 @@
 var correctKey = "dai2k4bn";
 var formShown = false;
-document.getElementById("avatar").addEventListener("click", function() {
-    if (formShown) {
-        document.getElementById("key-form").style.display = "none";
-            formShown = false;
+document.getElementById("avatar").addEventListener("click", function () {
+  if (formShown) {
+    document.getElementById("key-form").style.display = "none";
+    formShown = false;
+  } else {
+    document.getElementById("key-form").style.display = "block";
+    formShown = true;
+  }
+});
+document
+  .getElementById("key-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    var key = document.getElementById("key-input").value;
+    if (key === correctKey) {
+      window.location.href =
+        "https://drive.google.com/drive/folders/1AkG7LJMA-9mC_nqlXQSZTWz84t4wC6iw?usp=sharing";
     } else {
-        document.getElementById("key-form").style.display = "block";
-            formShown = true;
+      alert("Wrong key. Please try again.");
     }
-    });
-    document.getElementById("key-form").addEventListener("submit", function(event) {
-        event.preventDefault();
-        var key = document.getElementById("key-input").value;
-        if (key === correctKey) {
-            window.location.href = "https://drive.google.com/drive/folders/1AkG7LJMA-9mC_nqlXQSZTWz84t4wC6iw?usp=sharing";
-        } else {
-            alert("Wrong key. Please try again.");
-        }
-    });
+  });
+function copyText(text) {
+  navigator.clipboard.writeText(text).then(
+    function () {
+      alert("copied " + text + " successfully");
+    },
+    function () {
+      alert("Copy failed. Please try again.");
+    }
+  );
+}
